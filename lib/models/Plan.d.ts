39cabf68effba5a772268bcbb5717279747f8eaa
LeonardoTonizza <leonardo.tonizza@hotmail.com>
2024-07-30 01:17:52 -0300

    ..

--- lib/models/Plan.d.ts
+++ lib/models/Plan.d.ts
@@ -1,12 +1,14 @@
 import { PaymentMethod } from '../typings/PaymentMethod.js';
 export interface Plan {
-    id: string;
     billing_type: string;
     currency: string;
     description: string;
+    id: string;
     installments: number[];
     interval_count: number;
     interval: string;
+    metadata: Record<string, unknown>;
+    minimum_price: number;
     name: string;
     payment_methods: PaymentMethod[];
     status: string;
